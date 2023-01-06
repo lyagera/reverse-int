f55bde8890e7ba988d30083038d55364423f7087
lyagera <slyagera@gmail.com>
2023-01-06 21:04:39 +0200

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(Math.abs(n));
+    const rev = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev.push(str[i]);
+    }
+    return Number(rev.join(""));
+};
+// console.log(reverse(-291));
